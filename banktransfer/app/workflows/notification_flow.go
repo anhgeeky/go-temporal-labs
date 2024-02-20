b69c6da2909abe58cf0b08330d1a5ec485827836
Anh Geeky <anhgeeky@gmail.com>
2024-02-20 11:23:40 +0700

    Add workflows

--- banktransfer/app/workflows/notification_flow.go
+++ banktransfer/app/workflows/notification_flow.go
@@ -1,3 +1,27 @@
 package workflows
 
+import (
+	"github.com/anhgeeky/go-temporal-labs/banktransfer/app/messages"
+	"go.temporal.io/sdk/workflow"
+)
+
+// ================================================
 // Luồng gửi thông báo
+// ================================================
+
+func NotificationWorkflow(ctx workflow.Context, state messages.NotificationMessage) error {
+	// https://docs.temporal.io/docs/concepts/workflows/#workflows-have-options
+	logger := workflow.GetLogger(ctx)
+
+	err := workflow.SetQueryHandler(ctx, "getNotification", func(input []byte) (messages.NotificationMessage, error) {
+		return state, nil
+	})
+	if err != nil {
+		logger.Info("SetQueryHandler failed.", "Error", err)
+		return err
+	}
+
+	// TODO: Bổ sung
+
+	return nil
+}
--- banktransfer/app/workflows/verify_workflow.go
+++ banktransfer/app/workflows/verify_workflow.go
@@ -1,3 +1,27 @@
 package workflows
 
+import (
+	"github.com/anhgeeky/go-temporal-labs/banktransfer/app/messages"
+	"go.temporal.io/sdk/workflow"
+)
+
+// ================================================
 // Xác thực trước khi chạy 1 luồng xử lý
+// ================================================
+
+func VerifyWorkflow(ctx workflow.Context, state messages.VerifyMessage) error {
+	// https://docs.temporal.io/docs/concepts/workflows/#workflows-have-options
+	logger := workflow.GetLogger(ctx)
+
+	err := workflow.SetQueryHandler(ctx, "getVerify", func(input []byte) (messages.VerifyMessage, error) {
+		return state, nil
+	})
+	if err != nil {
+		logger.Info("SetQueryHandler failed.", "Error", err)
+		return err
+	}
+
+	// TODO: Bổ sung
+
+	return nil
+}
